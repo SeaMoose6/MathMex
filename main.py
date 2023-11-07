0ec1b0d9bebd903ce197848296fb52e39db84807
SeaMoose6 <89918967+SeaMoose6@users.noreply.github.com>
2023-11-07 15:22:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import requests
+import wikipediaapi
 from bs4 import BeautifulSoup
+from googletrans import Translator
 
 def language(lang):
-    math_link = "https://en.wikipedia.org/wiki/Pythagorean_theorem"
+    english_name = "Pythagorean_theorem"
+    spanish_name = "Teorema_de_Pitágoras"
+    fr_name = "héorème_de_Pythagore"
+    zh_name = "勾股定理"
+    if lang == "en":
+      name = english_name
+    elif lang == "es":
+      name = spanish_name
+    elif lang == "fr":
+      name = fr_name
+    elif lang == "zh":
+      name = zh_name
+    english_name = "Pythagorean_theorem"
+    spanish_name = "Teorema_de_Pitágoras"
+    math_link = f"https://{lang}.wikipedia.org/wiki/{name}"
     response = requests.get(math_link)
-    soup = BeautifulSoup(response.content, "html.parser")
-    text = soup.find('p')
-    print(text.txt)
+    soup = BeautifulSoup(response.text, "html.parser")
+    text = soup.find_all('p')
+    print(text[1:2])
 
-language("spanish")
-
+#language("en")
+language("es")
